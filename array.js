05946d3b81b5f1aed37d0ae63b019dd2aa1884f0
Christopher Hutchinson <cshutchinson@gmail.com>
2015-10-28 08:56:50 -0600

    created object and array as needed

---
+++ array.js
@@ -0,0 +1,14 @@
+var person  = {
+  lastName: 'Hutchinson',
+  firtName: 'Christopher',
+  middleName: 'Scott',
+  address: '1129 Village Circle',
+  city: 'Erie',
+  state: 'Colorado',
+  zip: '80516'
+}
+
+var likes = ["programming", "fishing", "mountain trails", "reading"];
+
+console.log(person);
+console.log(likes);
